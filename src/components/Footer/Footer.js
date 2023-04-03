9a6b74139007988fb25133a4b574d9f45097f915
aritos20 <agianicopulos@gmail.com>
2023-04-03 12:57:04 +0200

    changes are done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/aritos20'
+      className='link footer__link'
+    >
+      Aristidis Gianicopulos
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,88 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://aritos20.github.io/myPortfolio',
+  title: 'AG.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Aristidis Gianicopulos',
+  role: 'Desarrollador Web Full-Stack',
+  description:
+    'Hola, soy Aristidis Gianicopulos, Desarrollador Web Full-Stack. Me considero una persona con ganas de nunca parar de aprender y adaptarme a distintos entornos para así poder enriquecerme en todos los aspectos. También me gusta el trabajo en equipo y poder formar parte de grupos de trabajo para así poder aportar mi pequeño grano de arena y que los demás me aporten a mi sus conocimientos',
+  resume: 'https://drive.google.com/file/d/1oVrLl7XtWjf9swpFqFMcoi1sWIQbDrxA/view?usp=share_link',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/agianico/',
+    github: 'https://github.com/aritos20',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Hotel Miranda Web Page',
+    description:
+      'Página web de un hotel lujoso, es una MPA con distintos apartados y hecha por un diseño a partir de FIGMA',
+    stack: ['HTML', 'CSS', 'SASS', 'JavaScript'],
+    sourceCode: 'https://github.com/aritos20/hotel-miranda-web-page',
+    livePreview: 'https://aritos20.github.io/hotel-miranda-web-page/',
+  },
+  {
+    name: 'Hotel Miranda Dashboard',
+    description:
+      'Página para poder gestionar el Back Office de un hotel desde las reservas hasta los empleados.',
+    stack: ['React', 'Redux', 'Styled Components'],
+    sourceCode: 'https://github.com/aritos20/hotel-miranda-dashboard',
+    livePreview: 'https://aritos20.github.io/hotel-miranda-dashboard/',
+  },
+  {
+    name: 'Hotel Miranda Backend',
+    description:
+      'Es una API REST hecha con Node y Express para poder gestionar todos los datos y hacer la conexión con el Dashboard',
+    stack: ['TypeScript', 'Node', 'Express', 'Mongo', 'JWT'],
+    sourceCode: 'https://github.com/aritos20/hotel-miranda-backend',
+  },
+  {
+    name: 'PhotoSnap',
+    description:
+      'Aplicación Web para buscar y descargar imagenes de Stock. las imágenes que se obtienen provienen de Unsplash mediante su API',
+    stack: ['React', 'Redux', 'MUI'],
+    sourceCode: 'https://github.com/aritos20/photo-snap',
+    livePreview: 'https://aritos20.github.io/photo-snap/',
+  },
+  {
+    name: 'Oxygen Shop',
+    description:
+      'Landing Page básica hecha con HTML, CSS, SASS y JavaScript, es 100% responsive',
+    stack: ['HTML', 'CSS', 'SASS', 'JavaScript'],
+    sourceCode: 'https://github.com/aritos20/photo-snap',
+    livePreview: 'https://aritos20.github.io/photo-snap/',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'SASS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'NodeJS', 
+  'Express',
+  'MongoDB',
+  'MySql',
+  'Git',
+  'Jest',
+  'Figma',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'agianicopulos@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
