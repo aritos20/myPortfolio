a6acb722701e4096c2baa2b8f2f3dd81a5160c51
aritos20 <agianicopulos@gmail.com>
2023-04-11 17:45:53 +0200

    foto de perfil añadida

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,7 @@ import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
+import fotoPerfil from '../../assets/foto_perfil_42-removebg-preview.png';
 
 const About = () => {
   const { name, role, description, resume, social } = about
@@ -15,6 +16,9 @@ const About = () => {
       )}
 
       {role && <h2 className='about__role'>{role}.</h2>}
+
+      <img src={fotoPerfil} style={{borderRadius: '35%', width: '325px', height: '375px', marginTop: '24px'}}></img>
+
       <p className='about__desc'>{description && description}</p>
 
       <div className='about__contact center'>
