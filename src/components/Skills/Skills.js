2c825b9ca1a17211c965c887b03ddef8d365fc25
aritos20 <agianicopulos@gmail.com>
2023-04-11 16:46:10 +0200

    icons added to skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,6 +1,27 @@
 import uniqid from 'uniqid'
 import { skills } from '../../portfolio'
-import './Skills.css'
+import './Skills.css';
+import { AiFillHtml5 } from 'react-icons/ai';
+import { SiCss3, SiSass, SiJavascript, SiTypescript, SiReact, SiRedux, SiNodedotjs, SiExpress, SiMongodb, SiMysql, SiGit, SiJest, SiFigma } from 'react-icons/si';
+
+const width = '150px';
+
+const skillsIcons = [
+  {name: 'HTML', icon: <AiFillHtml5 />},
+  {name: 'CSS', icon: <SiCss3 />},
+  {name: 'SASS', icon: <SiSass />},
+  {name: 'JavaScript', icon: <SiJavascript />},
+  {name: 'TypeScript', icon: <SiTypescript />},
+  {name: 'React', icon: <SiReact />},
+  {name: 'Redux', icon: <SiRedux />},
+  {name: 'NodeJS', icon: <SiNodedotjs />},
+  {name: 'Express', icon: <SiExpress />},
+  {name: 'MongoDB', icon: <SiMongodb />},
+  {name: 'MySql', icon: <SiMysql />},
+  {name: 'Git', icon: <SiGit />},
+  {name: 'Jest', icon: <SiJest />},
+  {name: 'Figma', icon: <SiFigma />},
+]
 
 const Skills = () => {
   if (!skills.length) return null
@@ -9,9 +30,9 @@ const Skills = () => {
     <section className='section skills' id='skills'>
       <h2 className='section__title'>Skills</h2>
       <ul className='skills__list'>
-        {skills.map((skill) => (
-          <li key={uniqid()} className='skills__list-item btn btn--plain'>
-            {skill}
+        {skillsIcons.map((skill) => (
+          <li key={uniqid()} className='skills__list-item btn btn--plain' style={{display: 'flex', alignItems: 'center', gap: '8px'}}>
+            {skill.icon}{skill.name}
           </li>
         ))}
       </ul>
